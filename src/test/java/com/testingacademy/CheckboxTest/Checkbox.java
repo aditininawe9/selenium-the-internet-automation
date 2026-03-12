065e129123048e9c8d700bb3489c4e10e2a25ef1
Aditi Soni <aditininawe9@gmail.com>
2026-03-13 04:35:30 +0530

    Automated Checkboxes

--- src/test/java/com/testingacademy/CheckboxTest/Checkbox.java
+++ src/test/java/com/testingacademy/CheckboxTest/Checkbox.java
@@ -1,4 +1,29 @@
 package com.testingacademy.CheckboxTest;
 
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Checkbox {
+    @Test
+    @Description("Verify Checkboxes")
+    public void verifyCheckboxes() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+        WebElement checkbox1 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]"));
+        if(!checkbox1.isSelected()) {
+            checkbox1.click();
+        }
+        if(!checkbox2.isSelected()){
+            checkbox2.click();
+        }
+        Assert.assertTrue(checkbox1.isSelected(),"Checkbox 1 is selected");
+        Assert.assertTrue(checkbox2.isSelected(),"Checkbox 2 is selected");
+    }
 }
